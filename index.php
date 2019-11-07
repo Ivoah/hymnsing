540b7e0f94c672c9d191d3aeae75e114632281a5
Ian Spryn <4182500+ianspryn@users.noreply.github.com>
2019-11-07 15:00:33 -0500

    Basic navbar

--- index.php
+++ index.php
@@ -5,48 +5,57 @@ ini_set('display_errors', 'On');
 
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Trinity Hymnal</title>
-        <link rel="stylesheet" type="text/css" href="style.css">
 
-        <script src="https://unpkg.com/feather-icons"></script>
+<head>
+    <title>Trinity Hymnal</title>
+    <!-- style -->
+    <link rel="stylesheet" type="text/css" href="style.css">
 
-        <nav class="topnav" id="myTopnav">
-            <a href="#home" class="active">Home</a>
-            <a href="#news">News</a>
-            <a href="#contact">Contact</a>
-            <a href="#about">About</a>
-            <a href="javascript:void(0);" class="icon" onclick="myFunction()">
-                <i class="fa fa-bars"></i>
-            </a>
-        </nav>
-    </head>
-    <body>
-        <h1><a href="/">Trinity Hymnal</a></h1>
-        <a href="/history">History</a>
-        <hr>
-        <table>
-            <?php
-                include("auth.php");
+    <!-- fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway&display=swap" rel="stylesheet">
 
-                $db = new mysqli($dbhost, $dbuser, $dbpassword, $dbdb);
-                if ($db->connect_errno) {
-                    die("Connection failed: " . $db->connect_error);
-                }
+    <!-- icons -->
+    <script src="https://unpkg.com/feather-icons"></script>
 
-                $hymns = $db->query("SELECT * from hymns");
+    <!-- top nav bar -->
+    <div class="navBarWrapper">
+        <div class="top bar">
+            <ul class="left">
+                <li class="title"><a href="">Trinity Hymnal</a></li>
+            </ul>
+            <ul class="right">
+                <li class="buttons"><a class="iconLink" href="">History</a></li>
+                <li class="buttons"> <a href="" class="otherPages">Admin</a></li>
+            </ul>
+        </div>
+    </div>
+</head>
 
-                $sec = "";
-                $subsec = "";
-                while ($hymn = $hymns->fetch_assoc()) {
-                    if ($hymn["section"] != $sec || $hymn["subsection"] != $subsec) {
-                        $sec = $hymn["section"];
-                        $subsec = $hymn["subsection"];
-                        echo("</table><h3>$sec: $subsec</h3><table>");
-                    }
-                    echo("<tr><td>{$hymn["num"]}</td><td><a href=\"{$hymn["num"]}\">{$hymn["title"]}</a></td>\n");
-                }
-            ?>
-        </table>
-    </body>
+<body>
+    <hr>
+    <table>
+        <?php
+        include("auth.php");
+
+        $db = new mysqli($dbhost, $dbuser, $dbpassword, $dbdb);
+        if ($db->connect_errno) {
+            die("Connection failed: " . $db->connect_error);
+        }
+
+        $hymns = $db->query("SELECT * from hymns");
+
+        $sec = "";
+        $subsec = "";
+        while ($hymn = $hymns->fetch_assoc()) {
+            if ($hymn["section"] != $sec || $hymn["subsection"] != $subsec) {
+                $sec = $hymn["section"];
+                $subsec = $hymn["subsection"];
+                echo ("</table><h3>$sec: $subsec</h3><table>");
+            }
+            echo ("<tr><td>{$hymn["num"]}</td><td><a href=\"{$hymn["num"]}\">{$hymn["title"]}</a></td>\n");
+        }
+        ?>
+    </table>
+</body>
+
 </html>
