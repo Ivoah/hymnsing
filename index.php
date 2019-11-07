d6d51abe369b2c393d658331fc72543137259d9a
Ian Spryn <4182500+ianspryn@users.noreply.github.com>
2019-11-07 13:55:47 -0500

    Really tiny stuff

--- index.php
+++ index.php
@@ -1,8 +1,22 @@
 <html>
-    <head>
-        <title>Trinity Hymnal</title>
-    </head>
-    <body>
-        <h1>Trinity Hymnal</h1>
-    </body>
+
+<head>
+    <title>Trinity Hymnal</title>
+    <script src="https://unpkg.com/feather-icons"></script>
+
+    <nav class="topnav" id="myTopnav">
+        <a href="#home" class="active">Home</a>
+        <a href="#news">News</a>
+        <a href="#contact">Contact</a>
+        <a href="#about">About</a>
+        <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+            <i class="fa fa-bars"></i>
+        </a>
+    </nav>
+</head>
+
+<body>
+    <h1>Trinity Hymnal</h1>
+</body>
+
 </html>
