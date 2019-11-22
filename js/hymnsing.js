04ca5e06c6f329577d1bd2e13779b04f4effe1f0
Noah Rosamilia <ivoahivoah@gmail.com>
2019-11-21 19:55:54 -0500

    Update all likes on page when liking/unliking

--- js/hymnsing.js
+++ js/hymnsing.js
@@ -11,19 +11,19 @@ $(document).ready(function () {
   });
 
   $(".heart").on("click", function() {
-    var heart = $(this);
-    var label = heart.next();
-    var likes = parseInt(label.text().split());
-    var num = heart.attr("num");
+    var num = $(this).attr("num");
+    var hearts = $(`.heart[num="${num}"]`);
+    var labels = hearts.next();
+    var likes = parseInt(labels.text().split());
 
     function addLikes(update) {
       likes += update;
-      heart.toggleClass("liked");
-      label.text(likes + " like" + (likes != 1 ? "s" : ""));
+      hearts.toggleClass("liked");
+      labels.text(likes + " like" + (likes != 1 ? "s" : ""));
     }
 
-    if (!(heart.hasClass("liked"))) {
-      $.post("/like/" + num, () => {heart.toggleClass("is_animating"); addLikes(1)}).fail(() => alert("Could not update like"));
+    if (!($(this).hasClass("liked"))) {
+      $.post("/like/" + num, () => {$(this).toggleClass("is_animating"); addLikes(1)}).fail(() => alert("Could not update like"));
     } else {
       $.post("/unlike/" + num, () => addLikes(-1)).fail(() => alert("Could not update like"));
     }
