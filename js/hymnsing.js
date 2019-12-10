83806a5a89a38f40e47440b41282532afc1c1c02
Ian Spryn <4182500+ianspryn@users.noreply.github.com>
2019-12-09 23:19:29 -0500

    Don't search "# like(s)", id-navgation shifted properly
    The search bar will no longer look at 4th column containing the number of likes when searching for words
    
    When clicking a song's history date and it takes the user to that location on the page, it will now automatically scroll up a little bit to counteract the height of the navigation bar

--- js/hymnsing.js
+++ js/hymnsing.js
@@ -2,9 +2,9 @@ function adminLogin() {
   var pw = $("#input").val();
   $.post("/login", {
     "password": pw
-  }).done(function() {
+  }).done(function () {
     window.location.href = "/";
-  }).fail(function() {
+  }).fail(function () {
     alert("Invalid login");
   });
 }
@@ -16,7 +16,7 @@ function addHymn(num) {
       $.post("/addHymn", {
         date: date,
         hymn: num
-      }).done(function() {
+      }).done(function () {
         // if ($("table").children().size() == 0) {
         //   $("table").html(`
         //     <tr>
@@ -27,9 +27,9 @@ function addHymn(num) {
         //   var top = $("table");
         // }
         location.reload();
-      }).fail(function() {
+      }).fail(function () {
         alert("Could not add hymn");
-      }); 
+      });
     } else {
       alert("Please enter a date that is not in the future")
     }
@@ -38,22 +38,28 @@ function addHymn(num) {
   }
 }
 
+function offsetAnchor() {
+  if (location.hash.length !== 0) {
+    window.scrollTo(window.scrollX, window.scrollY - 80);
+  }
+}
+
 $(document).ready(function () {
   $("#datepicker").datepicker();
   $("#datepicker").datepicker("option", "dateFormat", "yy-mm-dd");
 
-  $("input[type=search]").on("keyup", function() {
+  $("input[type=search]").on("keyup", function () {
     var query = $(this).val().toLowerCase();
-    $("#hymn-tables tbody>tr").each(function() {
-      $(this).toggleClass("hidden", $(this).text().toLowerCase().indexOf(query) === -1);
+    $("#hymn-tables tbody>tr").each(function () {
+      $(this).toggleClass("hidden", $(this).find("td.searchable").text().toLowerCase().indexOf(query) === -1);
     });
 
-    $("#hymn-tables table").each(function() {
+    $("#hymn-tables table").each(function () {
       $(this).toggleClass("hidden", $(this).find("tbody tr:not(.hidden)").length === 0);
     });
   });
 
-  $(".heart").on("click", function() {
+  $(".heart").on("click", function () {
     var num = $(this).attr("num");
     var hearts = $(`.heart[num="${num}"]`);
     var labels = hearts.next();
@@ -66,13 +72,26 @@ $(document).ready(function () {
     }
 
     if (!($(this).hasClass("liked"))) {
-      $.post("/like/" + num, () => {$(this).toggleClass("is_animating"); addLikes(1)}).fail(() => alert("Could not update like"));
+      $.post("/like/" + num, () => { $(this).toggleClass("is_animating"); addLikes(1) }).fail(() => alert("Could not update like"));
     } else {
       $.post("/unlike/" + num, () => addLikes(-1)).fail(() => alert("Could not update like"));
     }
   });
 
-  $(".heart").on("animationend", function(){
+  $(".heart").on("animationend", function () {
     $(this).toggleClass("is_animating");
   });
+
+  // CREDIT: https://stackoverflow.com/questions/17534661/make-anchor-link-go-some-pixels-above-where-its-linked-to
+  // Captures click events of all <a> elements with href starting with #
+  $(document).on('click', 'a[href^="#"]', function (event) {
+    // Click events are captured before hashchanges. Timeout
+    // causes offsetAnchor to be called after the page jump.
+    window.setTimeout(function () {
+      offsetAnchor();
+    }, 0);
+  });
+
+  // Set the offset when entering page with hash present in the url
+  window.setTimeout(offsetAnchor, 0);
 });
--- templates/innertable.tpl
+++ templates/innertable.tpl
@@ -1,6 +1,6 @@
 <tr>
-        <td class="clickable" onclick="window.location.href = '{{hymn['num']}}'">{{hymn['num']}}</td>
-        <td class="clickable" onclick="window.location.href = '{{hymn['num']}}'">{{hymn['title']}}</td>
+        <td class="clickable searchable" onclick="window.location.href = '{{hymn['num']}}'">{{hymn['num']}}</td>
+        <td class="clickable searchable" onclick="window.location.href = '{{hymn['num']}}'">{{hymn['title']}}</td>
         <td class="clickable heart {{'liked' if hymn['num'] in likes else ''}}" num={{hymn['num']}}></td>
         <td>{{hymn['likes'] or 0}} like{{'' if hymn['likes'] == 1 else 's'}}</td>
 </tr>
